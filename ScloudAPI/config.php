7401874f38797473a28178d8580179bdc2e56431
CloudSweet <cloud.sweet@yahoo.com>
2019-08-20 10:42:32 +0800

    Moved API to another project

--- ScloudAPI/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_port = 3306;
-$db_username = 'root';
-$db_password = 'password';
-$db_name = 'db_name';
-$cc_encryption_hash = 'cc_encryption_hash';
-$encryption_hash = 'Node12345678';
